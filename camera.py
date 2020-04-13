fba5d63ed54808d4dccb13e0a2dfbba5f9129fcd
Atul Verma <44652100+Atulbargotra@users.noreply.github.com>
2020-04-14 04:50:07 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -3,12 +3,12 @@ import cv2
 import numpy as np
 cam = cv2.VideoCapture(0)
 cv2.namedWindow('Original', cv2.WINDOW_NORMAL)
-#URL = "#curl -F "file=@opencv_frame_7.png" http://0.0.0.0:5000/"
+
 content_type = 'image/jpeg'
 headers = {'content-type': content_type}
 def sendtoserver(frame):
     r, image = cv2.imencode('.jpg', frame)
-    response = requests.post("http://192.168.29.37:5000/", data=image.tostring(), headers=headers)
+    response = requests.post("http://0.0.0.0:5000/", data=image.tostring(), headers=headers)
     return response 
 while True:
     ret, frame = cam.read()
